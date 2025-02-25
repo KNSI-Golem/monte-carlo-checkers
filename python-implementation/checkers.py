e3d8451f01aed74ac84e0f17afd89c016d79d7a0
Michał Bloch <112960926+Dnafivuq@users.noreply.github.com>
2025-02-25 01:06:54 +0100

    fixed docsstrings formatting to avoid merge conflics

--- python-implementation/checkers.py
+++ python-implementation/checkers.py
@@ -3,17 +3,17 @@ from game_simulation import GameSimulation
 
 
 class Checkers(GameSimulation):
-    '''
+    """
     BIG QUESTION - multimoves are possible in a game of checkers, when
     leaping over multiple pieces in succession. 
     Should we store this a single move, or rather as a sequence?
-    '''
+    """
 
     def is_terminal(self, game_state: GameState) -> str | None:
-        '''
+        """
         Returns name of Player ('B' or 'W') who won the game, if one won the game.
         If game isn't over, returns None.
-        '''
+        """
         pass
 
     def get_moves(self, game_state: GameState) -> list[GameState]:
