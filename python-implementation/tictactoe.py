3050c3c85a752ef036a81fc5c3f64b34df6383f8
Aleksander Nowak <olek.nowak.20@gmail.com>
2025-02-26 23:35:17 +0100

    Fixed typo

--- python-implementation/tictactoe.py
+++ python-implementation/tictactoe.py
@@ -25,32 +25,52 @@ class TicTacToe(GameSimulation):
     def __init__(self, board_size: int = 3) -> None:
         self.board_size = board_size
         self._indexes = dict()
-        self._calucalte_checking_indexes()
+        self._calculate_checking_indexes()
 
-    def _calucalte_checking_indexes(self):
-        self._indexes['rows'] = [[x+y*self.board_size for x in range(self.board_size)] for y in range(self.board_size)]
-        self._indexes['columns'] = [[x+y*self.board_size for y in range(self.board_size)] for x in range(self.board_size)]
-        self._indexes['diagonals'] = [
-            [[x+y*self.board_size for x in range(self.board_size) if x == y][0] for y in range(self.board_size)],
-            [[x+y*self.board_size for x in range(self.board_size) if x+y == self.board_size-1][0] for y in range(self.board_size)]
+    def _calculate_checking_indexes(self):
+        self._indexes["rows"] = [
+            [x + y * self.board_size for x in range(self.board_size)]
+            for y in range(self.board_size)
+        ]
+        self._indexes["columns"] = [
+            [x + y * self.board_size for y in range(self.board_size)]
+            for x in range(self.board_size)
+        ]
+        self._indexes["diagonals"] = [
+            [
+                [x + y * self.board_size for x in range(self.board_size) if x == y][0]
+                for y in range(self.board_size)
+            ],
+            [
+                [
+                    x + y * self.board_size
+                    for x in range(self.board_size)
+                    if x + y == self.board_size - 1
+                ][0]
+                for y in range(self.board_size)
+            ],
         ]
 
     def reward(self, game_state: GameState, desired_winner: Player) -> int | None:
         # check rows
-        for row in self._indexes['rows']:
+        for row in self._indexes["rows"]:
             row_sum = sum([game_state.board[slot_index].value for slot_index in row])
-            if (abs(row_sum) == self.board_size):
+            if abs(row_sum) == self.board_size:
                 return 1 if (np.sign(row_sum) == desired_winner.value) else -1
         # check columns
-        for column in self._indexes['columns']:
-            column_sum = sum([game_state.board[slot_index].value for slot_index in column])
-            if (abs(column_sum) == self.board_size):
+        for column in self._indexes["columns"]:
+            column_sum = sum(
+                [game_state.board[slot_index].value for slot_index in column]
+            )
+            if abs(column_sum) == self.board_size:
                 return 1 if (np.sign(column_sum) == desired_winner.value) else -1
 
         # check diagonals
-        for diagonal in self._indexes['diagonals']:
-            diagonal_sum = sum([game_state.board[slot_index].value for slot_index in diagonal])
-            if (abs(diagonal_sum) == self.board_size):
+        for diagonal in self._indexes["diagonals"]:
+            diagonal_sum = sum(
+                [game_state.board[slot_index].value for slot_index in diagonal]
+            )
+            if abs(diagonal_sum) == self.board_size:
                 return 1 if (np.sign(diagonal_sum) == desired_winner.value) else -1
 
         # check if there are empty slots, if so -> no winner but game still goes on
@@ -79,7 +99,7 @@ class TicTacToe(GameSimulation):
         """
         index = int(move)
         game_state.board[index] = BoardSlot(game_state.active_player.value)
-        game_state.active_player = Player(game_state.active_player.value*-1)
+        game_state.active_player = Player(game_state.active_player.value * -1)
         return game_state
 
     def make_random_move(self, game_state: GameState) -> GameState:
@@ -88,7 +108,9 @@ class TicTacToe(GameSimulation):
         return self.make_move(game_state, possible_moves[move_index])
 
     def get_starting_state(self) -> GameState:
-        return GameState([BoardSlot(0) for _ in range(self.board_size**2)], Player.CROSS)
+        return GameState(
+            [BoardSlot(0) for _ in range(self.board_size**2)], Player.CROSS
+        )
 
     def print_board(self, game_state: GameState) -> None:
         top = " ┌───┬───┬───┬───┐\n"
@@ -96,10 +118,14 @@ class TicTacToe(GameSimulation):
         spacer = " ├───┼───┼───┼───┤\n"
         bot = " └───┴───┴───┴───┘\n"
         rows = []
-        square_triplets = [game_state.board[i:i+3] for i in range(0, len(game_state.board), 3)]
+        square_triplets = [
+            game_state.board[i : i + 3] for i in range(0, len(game_state.board), 3)
+        ]
         for row_index, row in enumerate(square_triplets):
             s1 = " " if row[0].value == 0 else "X" if row[0].value == 1 else "O"
             s2 = " " if row[1].value == 0 else "X" if row[1].value == 1 else "O"
             s3 = " " if row[2].value == 0 else "X" if row[2].value == 1 else "O"
-            rows.append(f" │ {row_index+1} │ {s1} │ {s2} │ {s3} │\n")
-        print(top + legend + spacer + rows[0] + spacer + rows[1] + spacer + rows[2] + bot)
+            rows.append(f" │ {row_index + 1} │ {s1} │ {s2} │ {s3} │\n")
+        print(
+            top + legend + spacer + rows[0] + spacer + rows[1] + spacer + rows[2] + bot
+        )
