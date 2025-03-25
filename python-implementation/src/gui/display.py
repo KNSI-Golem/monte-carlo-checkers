f2fbc60c0473abe0a12a1ebe1a299241ac218213
szczaww <01187289@pw.edu.pl>
2025-03-25 13:09:01 +0100

    converted displat to object

--- python-implementation/src/gui/display.py
+++ python-implementation/src/gui/display.py
@@ -1,65 +1,57 @@
 import pygame
 import sys
 import pkg_resources
-from ..checkers import CheckersPiece, CheckersBoard
+from ..checkers import CheckersPiece, CheckersState
 
-SQUARE_SIZE = 64
-WIDTH = HEIGHT = 8 * SQUARE_SIZE
+class Display:
+    def __init__(self, width, height):
+        self.width = width
+        self.height = height
+        self.square_size = width // 8 if width < height else height // 8
 
+        self.screen = self._init_screen(width, height)
 
-def load_image(image_name):
-    image_path = pkg_resources.resource_filename(__name__, f"assets/{image_name}")
-    return pygame.image.load(image_path)
-
-
-def get_piece_image(piece):
-    match (piece):
-        case CheckersPiece.BLACK:
-            return load_image("black_piece.png")
-        case CheckersPiece.WHITE:
-            return load_image("white_piece.png")
-        case CheckersPiece.BLACK_QUEEN:
-            return load_image("black_queen.png")
-        case CheckersPiece.WHITE_QUEEN:
-            return load_image("white_queen.png")
-        case CheckersPiece.EMPTY:
-            return None
+    def _load_image(self, image_name):
+        image_path = pkg_resources.resource_filename(__name__, f"assets/{image_name}")
+        return pygame.image.load(image_path)
 
 
-def draw_board(screen, board):
-    pieces = board.get_squares()
-    for i in range(8):
+    def _get_piece_image(self, piece: CheckersPiece):
+        match (piece):
+            case CheckersPiece.BLACK:
+                return self._load_image("black_piece.png")
+            case CheckersPiece.WHITE:
+                return self._load_image("white_piece.png")
+            case CheckersPiece.BLACK_QUEEN:
+                return self._load_image("black_queen.png")
+            case CheckersPiece.WHITE_QUEEN:
+                return self._load_image("white_queen.png")
+            case CheckersPiece.EMPTY:
+                return None
+
+
+    def draw_board(self, state: CheckersState):
+        pieces = state.get_board().get_squares()
         for j in range(8):
-            color = (255, 255, 255) if (i + j) % 2 == 0 else (0, 0, 0)
-            pygame.draw.rect(
-                screen,
-                color,
-                (i * SQUARE_SIZE, j * SQUARE_SIZE, SQUARE_SIZE, SQUARE_SIZE),
-            )
-            piece = pieces[i * 8 + j]
-            if piece:
-                piece_image = get_piece_image(piece.color)
-                screen.blit(piece_image, (i * SQUARE_SIZE, j * SQUARE_SIZE))
+            for i in range(8):
+                color = (255, 255, 255) if (i + j) % 2 != 0 else (0, 0, 0)
+                pygame.draw.rect(
+                    self.screen,
+                    color,
+                    (i * self.square_size, j * self.square_size, self.square_size, self.square_size),
+                )
+                if i % 2 == j % 2:
+                    indx = j * 4 + i // 2
+                    piece = pieces[indx]
+                    if piece != CheckersPiece.EMPTY:
+                        img = self._get_piece_image(piece)
+                        img = pygame.transform.scale(img, (self.square_size, self.square_size))
+                        self.screen.blit(img, (i * self.square_size, j * self.square_size))
+                    
+
+    def _init_screen(self, width, height):
+        screen = pygame.display.set_mode((width, height))
+        pygame.display.set_caption("Golemowe Warcaby")
+        return screen
 
 
-def create_screen():
-    screen = pygame.display.set_mode((WIDTH, HEIGHT))
-    pygame.display.set_caption("Golemowe Warcaby")
-    return screen
-
-
-def start_game():
-    """Starts the checkers game."""
-    pygame.init()
-    screen = create_screen()
-
-    while True:
-        for event in pygame.event.get():
-            if event.type == pygame.QUIT:
-                pygame.quit()
-                return
-
-        screen.fill((0, 0, 0))  # Fill the screen with black
-        draw_board(screen)  # Draw the checkers board
-
-        pygame.display.update()  # Update the display
