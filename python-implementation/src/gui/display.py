031c3c00a7fa30663140f6d6450ad75ac397e07c
szczaww <01187289@pw.edu.pl>
2025-03-25 14:54:02 +0100

    tweaks for consistentcy with original checkers

--- python-implementation/src/gui/display.py
+++ python-implementation/src/gui/display.py
@@ -4,19 +4,19 @@ import pkg_resources
 from ..checkers import CheckersPiece, CheckersState
 
 class Display:
-    def __init__(self, width, height):
+    def __init__(self, width: int, height: int):
         self.width = width
         self.height = height
         self.square_size = width // 8 if width < height else height // 8
-
+        self.highlighted_square = None
         self.screen = self._init_screen(width, height)
 
-    def _load_image(self, image_name):
+    def _load_image(self, image_name: str) -> pygame.image:
         image_path = pkg_resources.resource_filename(__name__, f"assets/{image_name}")
         return pygame.image.load(image_path)
 
 
-    def _get_piece_image(self, piece: CheckersPiece):
+    def _get_piece_image(self, piece: CheckersPiece) -> pygame.image:
         match (piece):
             case CheckersPiece.BLACK:
                 return self._load_image("black_piece.png")
@@ -30,17 +30,21 @@ class Display:
                 return None
 
 
-    def draw_board(self, state: CheckersState):
+    def draw_board(self, state: CheckersState) -> None:
+        self.screen.fill((0, 0, 0))
         pieces = state.get_board().get_squares()
         for j in range(8):
             for i in range(8):
-                color = (255, 255, 255) if (i + j) % 2 != 0 else (0, 0, 0)
+                if  (i, j) == self.highlighted_square:
+                    color = (255, 0, 0),
+                else:
+                    color = (255, 255, 255) if (i + j) % 2 != 0 else (0, 0, 0)
                 pygame.draw.rect(
                     self.screen,
                     color,
                     (i * self.square_size, j * self.square_size, self.square_size, self.square_size),
                 )
-                if i % 2 == j % 2:
+                if i % 2 != j % 2:
                     indx = j * 4 + i // 2
                     piece = pieces[indx]
                     if piece != CheckersPiece.EMPTY:
@@ -49,14 +53,16 @@ class Display:
                         x_pad, y_pad = self._calculate_padding(img, self.square_size, self.square_size)
                         self.screen.blit(img, (i * self.square_size + x_pad, j * self.square_size + y_pad))
                     
+    def highlight_square(self, cords: tuple[int, int]):
+        self.highlighted_square = cords
 
-    def _init_screen(self, width, height):
+    def _init_screen(self, width: int, height: int) -> pygame.Surface: 
         screen = pygame.display.set_mode((width, height))
         pygame.display.set_caption("Golemowe Warcaby")
         return screen
 
 
-    def _calculate_padding(self, img, new_width, new_height):
+    def _calculate_padding(self, img: pygame.image, new_width: int, new_height: int) -> tuple[int, int]:
         img_width, img_height = img.get_width(), img.get_height()
         target_width = int(0.75 * self.square_size)
         target_height = int(0.75 * self.square_size)
